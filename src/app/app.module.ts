288ac48bdabf1865e7b7b678410376b85d51e23b
LinhPT2108 <tuanlinho9o22o15@gmail.com>
2023-05-19 00:24:07 +0700

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { QuestionComponent } from './question/question.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ListCourseComponent } from './list-course/list-course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    QuestionComponent,
+    FooterComponent,
+    LoginComponent,
+    SignUpComponent,
+    ListCourseComponent
   ],
   imports: [
     BrowserModule,
